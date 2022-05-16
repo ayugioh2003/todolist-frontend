0e13cd51e9a85d83f1ceca87d8812aacdb78de77
Hazel Wu <tinazx056@gmail.com>
2022-05-17 00:05:25 +0800

    fix(CoverImage):  img src 移除 require 引入方式

--- src/components/CoverImage.vue
+++ src/components/CoverImage.vue
@@ -1,24 +1,16 @@
 <template>
 <div class="inline-block">
-  <img :src="image" class="mx-auto text-center" alt="Cover Image">
+  <img src="@/assets/index-left.png" class="mx-auto text-center" alt="Cover Image" />
 </div>
 </template>
 
 <script>
-import { ref } from 'vue';
 
 export default {
 
-  setup() {
-    const image = ref(require('@/assets/index-left.png'));
-
-    return {
-      image,
-    };
-  },
 };
 </script>
-
+<!--  -->
 <style>
 
 </style>

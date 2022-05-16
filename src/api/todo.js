c785c1e9fe489f815a9839b7dd2cbbf8bc097656
Hazel Wu <tinazx056@gmail.com>
2022-05-16 22:23:41 +0800

    feat: Add Todo API

--- src/api/todo.js
+++ src/api/todo.js
@@ -8,3 +8,37 @@ export function getTodoListAPI(data) {
     data
   })
 }
+
+// 新增 Todo
+export function createTodoAPI(data) {
+  return request({
+    url: '/todos',
+    method: 'post',
+    data
+  })
+}
+
+// 修改 Todo
+export function updateTodoAPI(id, data) {
+  return request({
+    url: `/todos/${id}`,
+    method: 'put',
+    data
+  })
+}
+
+// 刪除 Todo
+export function deleteTodoAPI(id) {
+  return request({
+    url: `/todos/${id}`,
+    method: 'delete',
+  })
+}
+
+// 切換 Todo 狀態已完成 / 完成
+export function toggleTodoAPI(id) {
+  return request({
+    url: `/todos/${id}/toggle`,
+    method: 'patch',
+  })
+}

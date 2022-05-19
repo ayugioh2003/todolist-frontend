45d141706f48f805699575f8e5ba8d5f10ea6e5b
Hazel Wu <tinazx056@gmail.com>
2022-05-20 01:39:15 +0800

    fix(eslint): 修復 vercel 部署錯誤

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,11 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
-    '@vue/airbnb',
   ],
   parserOptions: {
     parser: '@babel/eslint-parser',
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    semi: ['error', 'always'],
-    'import/extensions': ['error', 'never'],
-    'vue/multi-word-component-names': {},
-    'global-require': 'off',
+
   },
 };
